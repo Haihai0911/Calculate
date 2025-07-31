835b6e123af716a750d67f0eee9f8a9488e2d884
Haihai0911 <3527028551@qq.com>
2025-07-31 13:06:12 +0800

    Add files via upload

--- Calculate.java
+++ Calculate.java
@@ -8,17 +8,31 @@ public class Calculate {
         Scanner scanner = new Scanner(System.in);
         String filePath_Question = "D:\\IDEA\\workplace\\Calculate\\计算题.txt";
         String filePath_Answer = "D:\\IDEA\\workplace\\Calculate\\计算题答案.txt";
+
         System.out.println("需要的题目数量：");
         int num = scanner.nextInt();
+
+        System.out.println("需要的数：");
+        System.out.println("1：整数");
+        int method = scanner.nextInt();
+
         System.out.println("题目组成结构：");
-        System.out.println("1:全是加法；2：全是减法");
+        System.out.println("1:全是加法；2：全是减法；3：全是乘法；4：全是除法");
+
         int model = scanner.nextInt();
+        if (method == 1) {
+            method1(model,num,filePath_Question,filePath_Answer);
+        }
+
+    }
+
+    public static void method1(int model, int num, String filePath_Question, String filePath_Answer) {
         while (num != 0) {
             Maker maker = new Maker();
             if(model == 1) {
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Question,true))) {
                     // 写入内容
-                    writer.write(maker.andMake());
+                    writer.write(maker.integerAndMake());
                     writer.newLine(); // 换行
                 } catch (IOException e) {
                     // 处理异常（如路径错误、权限问题等）
@@ -27,7 +41,7 @@ public class Calculate {
 
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Answer,true))) {
                     // 写入内容
-                    writer.write(maker.andAnswer());
+                    writer.write(maker.integerAndAnswer());
                     writer.newLine(); // 换行
                 } catch (IOException e) {
                     // 处理异常（如路径错误、权限问题等）
@@ -38,7 +52,7 @@ public class Calculate {
             else if (model == 2) {
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Question,true))) {
                     // 写入内容
-                    writer.write(maker.minusMake());
+                    writer.write(maker.integerMinusMake());
                     writer.newLine(); // 换行
                 } catch (IOException e) {
                     // 处理异常（如路径错误、权限问题等）
@@ -47,7 +61,47 @@ public class Calculate {
 
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Answer,true))) {
                     // 写入内容
-                    writer.write(maker.minusAnswer());
+                    writer.write(maker.integerMinusAnswer());
+                    writer.newLine(); // 换行
+                } catch (IOException e) {
+                    // 处理异常（如路径错误、权限问题等）
+                    e.printStackTrace();
+                }
+                num--;
+            }
+            else if (model == 3) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Question,true))) {
+                    // 写入内容
+                    writer.write(maker.integerMultiplyMake());
+                    writer.newLine(); // 换行
+                } catch (IOException e) {
+                    // 处理异常（如路径错误、权限问题等）
+                    e.printStackTrace();
+                }
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Answer,true))) {
+                    // 写入内容
+                    writer.write(maker.integerMultiplyAnswer());
+                    writer.newLine(); // 换行
+                } catch (IOException e) {
+                    // 处理异常（如路径错误、权限问题等）
+                    e.printStackTrace();
+                }
+                num--;
+            }
+            else if (model == 4) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Question,true))) {
+                    // 写入内容
+                    writer.write(maker.integerDivideMake());
+                    writer.newLine(); // 换行
+                } catch (IOException e) {
+                    // 处理异常（如路径错误、权限问题等）
+                    e.printStackTrace();
+                }
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath_Answer,true))) {
+                    // 写入内容
+                    writer.write(maker.integerDivideAnswer());
                     writer.newLine(); // 换行
                 } catch (IOException e) {
                     // 处理异常（如路径错误、权限问题等）
--- Maker.java
+++ Maker.java
@@ -2,26 +2,27 @@ import javax.print.attribute.standard.PrinterMakeAndModel;
 import java.util.Random;
 
 public class Maker {
+
     int a = 0;
     int b = 0;
     public Maker() {}
-    public String andMake() {
+
+    public String integerAndMake() {
         Random rand = new Random();
         int a,b;
         a = rand.nextInt(1000);
         b = rand.nextInt(1000);
         this.a = a;
         this.b = b;
-        String result = a+"+"+b+"=";
-        return result;
+        return a+"+"+b+"=";
     }
 
-    public String andAnswer() {
+    public String integerAndAnswer() {
         int result = a+b;
         return a+"+"+b+"="+result;
     }
 
-    public String minusMake() {
+    public String integerMinusMake() {
         Random rand = new Random();
         int a,b;
         a = rand.nextInt(1000);
@@ -37,8 +38,48 @@ public class Maker {
         return this.a+"-"+this.b+"=";
     }
 
-    public String minusAnswer() {
+    public String integerMinusAnswer() {
         //格式
         return this.a+"-"+this.b+"="+(this.a-this.b);
+    }
+
+    public String integerMultiplyMake() {
+        Random rand = new Random();
+        int a,b;
+        a = rand.nextInt(100);
+        b = rand.nextInt(100);
+        this.a = a;
+        this.b = b;
+        return a+"*"+b+"=";
+    }
+
+    public String integerMultiplyAnswer() {
+        //
+        return this.a+"*"+this.b+"="+(this.a*this.b);
+    }
+
+    public String integerDivideMake() {
+        Random rand = new Random();
+        int a,b;
+        a = rand.nextInt(100);
+        b = rand.nextInt(100);
+        if(b>a) {
+            this.a = b;
+            this.b = a;
+        }
+        else {
+            this.a = a;
+            this.b = b;
+        }
+        return this.a*this.b+"/"+this.b+"=";
+    }
+
+    public String integerDivideAnswer() {
+        //
+        return this.a*this.b+"/"+this.b+"="+(this.a*this.b);
+    }
+
+    public String decimalAndMake() {
+        return "";
     }
 }
